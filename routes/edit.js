b4ff1777a7f1002a59d363dba113d642ecdfabfa
Rafael <rafaelferreiracosta26@gmail.com>
2019-10-08 19:16:42 -0300

    Adding 'map' page and fixing editing

--- routes/edit.js
+++ routes/edit.js
@@ -16,13 +16,13 @@ router.get('/:id', (req, res) =>{
 
 router.post('/', (req, res) => {
     var query = { '_id': req.body.id };
-    let newData;
+    let newData = {};
     
     newData.numero = req.body.num;
     newData.bloco = req.body.bloco;
     newData.alias = req.body.alias;
 
-    Point.findOneAndUpdate(query, newData, { upsert: true, useFindAndModify: false }, function (err, res) {
+    Point.findOneAndUpdate(query, newData, { upsert: true, useFindAndModify: false }, function (err, doc) {
         if (err) return res.send(500, { error: err });
         return res.send("succesfully saved");
     });
